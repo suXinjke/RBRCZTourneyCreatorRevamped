d08b150b37f1cef44f07f61234cec5f9a42e468d
suXin <suxinjke@gmail.com>
2019-03-19 11:43:54 +0700

    remove non used variables from main

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,6 @@ __webpack_public_path__ = 'http://127.0.0.1:8080/rbrcz_prototype/'
 
 Vue.config.productionTip = false
 
-const url_search_params = new URLSearchParams( window.location.search )
-
-const act = url_search_params.get( 'act' )
-
 const tournamentForm = document.getElementById( 'tournament' )
 
 if ( tournamentForm ) {
