17cb058f7bf5ac5ad64aa679c1f50ac58160fde2
suXin <suxinjke@gmail.com>
2019-03-27 11:34:22 +0700

    mark as version 2

--- src/App.tsx
+++ src/App.tsx
@@ -410,7 +410,7 @@ export default Vue.extend( {
                         ) }
                         </div>
                         <div style='margin-top: 8px'>
-                            <div>ver 1a</div>
+                            <div>ver 2</div>
                             <div>
                                 <a href='https://github.com/suXinjke/RBRCZTourneyCreatorRevamped'>GitHub </a> |
                                 <a href='https://twitter.com/suxinjke'> Twitter </a> |
